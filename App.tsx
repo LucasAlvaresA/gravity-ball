eae366a5df1e84981888502140ddc9c0d44929aa
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-01-12 02:44:25 -0300

    ✨ finish the project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
+import {Ball} from './components/Ball';
+
+let timer: any;
 
 const App = () => {
-  const handlePushButton = () => {};
+  const [gravity] = useState(0.98);
+  const [upForce, setUpForce] = useState(0);
+  const [speed, setSpeed] = useState(0);
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    const applyGravity = () => {
+      //Decreasing UpForce
+      let newUpForce = upForce - gravity;
+      newUpForce = newUpForce < 0 ? 0 : newUpForce;
+      setUpForce(newUpForce);
+
+      //Modifying speed
+      let newSpeed = speed + (gravity - newUpForce / 2);
+      setSpeed(newSpeed);
+
+      //Setting new position based on speed
+      let newPosition = position - newSpeed;
+
+      if (newPosition <= 0) {
+        newPosition = 0;
+        setSpeed(0);
+      }
+
+      setPosition(newPosition);
+    };
+
+    clearTimeout(timer);
+    timer = setTimeout(applyGravity, 5);
+  }, [gravity, upForce, speed, position]);
+
+  const handlePushButton = () => {
+    setUpForce(7);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.area}></View>
+      <View style={styles.area}>
+        <Ball posY={position} />
+      </View>
       <View style={styles.control}>
         <View>
-          <Text style={styles.controlText}>UpForce:</Text>
-          <Text style={styles.controlText}>Velocity:</Text>
-          <Text style={styles.controlText}>PosY:</Text>
+          <Text style={styles.controlText}>UpForce: {upForce.toFixed(2)}</Text>
+          <Text style={styles.controlText}>Speed: {speed.toFixed(2)}</Text>
+          <Text style={styles.controlText}>
+            Position: {position.toFixed(2)}
+          </Text>
         </View>
         <TouchableOpacity
           style={styles.controlButton}
